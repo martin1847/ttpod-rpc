6c4392a6af951efdca3d6c3cb0ebafe229404ecb
yangyang.cong <yangyang.cong@ttpod.com>
2014-02-17 17:12:08 +0800

    ServerProcessor.RegCenter

--- ttpod-rpc/src/test/java/test/netty/query/QueryServer.java
+++ ttpod-rpc/src/test/java/test/netty/query/QueryServer.java
@@ -7,6 +7,7 @@ import com.ttpod.rpc.ResponseBean;
 import com.ttpod.rpc.netty.pool.impl.DefaultGroupManager;
 import com.ttpod.rpc.netty.server.DefaultServerHandler;
 import com.ttpod.rpc.netty.server.DefaultServerInitializer;
+import com.ttpod.rpc.server.AbstractServerProcessor;
 import com.ttpod.rpc.server.ServerProcessor;
 import io.netty.util.Version;
 
@@ -26,19 +27,19 @@ public class QueryServer {
         );
 
         final DefaultServerHandler serverHandler = new DefaultServerHandler();
-        serverHandler.setProcessors(
-                new ServerProcessor[]{
-                        new ServerProcessor() {
-                            public ResponseBean handle(RequestBean req) throws Exception {
-                                ResponseBean data = new ResponseBean();
-                                data.setCode(1);
-                                data.setPages(10);
-                                data.setRows(2000);
-                                data.setData(Arrays.asList(new Pojo(req.getData(), 100), new Pojo("OK", 10)));
-                                return data;
-                            }
-                        }
+        ServerProcessor.RegCenter regCenter = ServerProcessor.RegCenter.getInstance();
+
+        regCenter.regProcessor(
+            new AbstractServerProcessor() {
+                public ResponseBean handle(RequestBean req) throws Exception {
+                    ResponseBean data = new ResponseBean();
+                    data.setCode(1);
+                    data.setPages(10);
+                    data.setRows(2000);
+                    data.setData(Arrays.asList(new Pojo(req.getData(), 100), new Pojo("OK", 10)));
+                    return data;
                 }
+            }
         );
         new Server(new DefaultServerInitializer(serverHandler),6666,new DefaultGroupManager(
               "192.168.8.12:2181","com.ttpod.search"
