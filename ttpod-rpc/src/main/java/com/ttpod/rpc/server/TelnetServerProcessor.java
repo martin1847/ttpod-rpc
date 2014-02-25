1cd97509bc681e1f363694250413c784fef50807
yangyang.cong <yangyang.cong@ttpod.com>
2014-02-25 18:12:38 +0800

    Throwable telnet

--- ttpod-rpc/src/main/java/com/ttpod/rpc/server/TelnetServerProcessor.java
+++ ttpod-rpc/src/main/java/com/ttpod/rpc/server/TelnetServerProcessor.java
@@ -2,6 +2,8 @@ package com.ttpod.rpc.server;
 
 import com.ttpod.rpc.RequestBean;
 import com.ttpod.rpc.ResponseBean;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * date: 14-2-18 下午2:52
@@ -10,12 +12,21 @@ import com.ttpod.rpc.ResponseBean;
  */
 public abstract class TelnetServerProcessor extends AbstractServerProcessor {
 
+    static final Logger logger = LoggerFactory.getLogger(TelnetServerProcessor.class);
+
     @Override
     public ResponseBean handle(RequestBean req) throws Exception {
-        String data = handle(req.getData());
-        ResponseBean res = ResponseBean.code1();
-        res.setData(data);
-        return res;
+        try{
+            String data = handle(req.getData());
+            ResponseBean res = ResponseBean.code1();
+            res.setData(data);
+            return res;
+        }catch (Throwable e){//For RPC must Notify Client .
+            logger.error("handleRequest Error ",e);
+            ResponseBean res = ResponseBean.error();
+            res.setData(e.getLocalizedMessage());
+            return res;
+        }
     }
 
     public abstract String handle(String arg) throws Exception;
