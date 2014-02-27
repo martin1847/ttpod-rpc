b69b4627a6d075bcb0603e39856b35973334166e
yangyang.cong <yangyang.cong@ttpod.com>
2014-02-27 16:10:08 +0800

    ExplicitIdStrategy.Registry BUG

--- ttpod-rpc/src/main/java/com/ttpod/rpc/netty/codec/ResponseDecoder.java
+++ ttpod-rpc/src/main/java/com/ttpod/rpc/netty/codec/ResponseDecoder.java
@@ -2,6 +2,8 @@ package com.ttpod.rpc.netty.codec;
 
 import com.dyuproject.protostuff.ProtostuffIOUtil;
 import com.dyuproject.protostuff.Schema;
+import com.dyuproject.protostuff.runtime.DefaultIdStrategy;
+import com.dyuproject.protostuff.runtime.IdStrategy;
 import com.dyuproject.protostuff.runtime.RuntimeSchema;
 import com.ttpod.rpc.ResponseBean;
 import io.netty.buffer.ByteBuf;
@@ -22,7 +24,36 @@ import java.util.List;
 public class ResponseDecoder extends MessageToMessageDecoder<ByteBuf> {
 
     static final Logger logger = LoggerFactory.getLogger(ResponseDecoder.class);
-    static final Schema<ResponseBean> schema =  RuntimeSchema.getSchema(ResponseBean.class);
+    public static final IdStrategy ID_STRATEGY = new DefaultIdStrategy();
+
+//    static {
+//        ExplicitIdStrategy.Registry r = new ExplicitIdStrategy.Registry();
+//        r.registerCollection(CollectionSchema.MessageFactories.ArrayList, 1)
+//                .registerCollection(CollectionSchema.MessageFactories.HashSet, 2)
+//                .registerCollection(CollectionSchema.MessageFactories.LinkedList, 3)
+//                .registerCollection(CollectionSchema.MessageFactories.TreeSet, 4)
+//                .registerCollection(CollectionSchema.MessageFactories.CopyOnWriteArrayList, 5);
+//
+//        r.registerMap(MapSchema.MessageFactories.HashMap, 1)
+//                .registerMap(MapSchema.MessageFactories.LinkedHashMap, 2)
+//                .registerMap(MapSchema.MessageFactories.TreeMap, 3)
+//                .registerMap(MapSchema.MessageFactories.Hashtable, 4)
+//        .registerMap(MapSchema.MessageFactories.ConcurrentHashMap, 5);
+//
+//        r.registerPojo(ResponseBean.class, 1)
+//        .registerPojo(RequestBean.class, 2)
+//        .registerPojo(NullPointerException.class, 3)
+//                .registerPojo(IOException.class, 4)
+//                .registerPojo(Throwable.class, 5)
+//                .registerPojo(Exception.class, 6)
+//                .registerPojo(RuntimeException.class, 7);
+//
+//        ID_STRATEGY = new DefaultIdStrategy(r.strategy,1);
+//    }
+
+    //https://code.google.com/p/protostuff/wiki/ProtostuffRuntime#Performance_guidelines
+
+    static final Schema<ResponseBean> schema =  RuntimeSchema.getSchema(ResponseBean.class,ID_STRATEGY);
 
     @Override
     protected void decode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
