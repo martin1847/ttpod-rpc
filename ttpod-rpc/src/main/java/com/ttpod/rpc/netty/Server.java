a252b9328cce5c3f1b9f2e99e1d8c73330e32251
yangyang.cong <yangyang.cong@ttpod.com>
2014-03-13 21:49:53 +0800

    startInNewThread

--- ttpod-rpc/src/main/java/com/ttpod/rpc/netty/Server.java
+++ ttpod-rpc/src/main/java/com/ttpod/rpc/netty/Server.java
@@ -81,14 +81,14 @@ public class Server {
     }
 
     private static int threadInitNumber;
-    
+
     public void startInNewThread(){
         new Thread(new Runnable() {
             @Override
             public void run() {
                 Server.this.start();
             }
-        },"Server.startInNewThread-" + threadInitNumber++);
+        },"Server.startInNewThread-" + threadInitNumber++).start();
         logger.info("Server Started in New Thread .");
     }
     public void shutdown(){
